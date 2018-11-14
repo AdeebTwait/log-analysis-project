2fe821711afe72d057cdc143421270bc9f0eef42
Adeeb Twait <adeebtwait@gmail.com>
2018-11-14 17:33:17 +0200

    "Format with pep8 guidelines"

--- log-project.py
+++ log-project.py
@@ -8,18 +8,17 @@ import psycopg2
 
 # Write the queries
 articles_query = '''
-          SELECT a.title, COUNT(*) AS views 
+          SELECT a.title, COUNT(*) AS views
           FROM articles AS a JOIN log AS l
           ON CONCAT('/article/', a.slug) = l.path
           GROUP BY a.title
           ORDER BY views DESC
           LIMIT 3;
-          
 '''
 
 authors_query = '''
           SELECT au.name, COUNT(*) AS views
-          FROM articles AS ar 
+          FROM articles AS ar
           JOIN authors AS au
                ON au.id = ar.author
           JOIN log AS l
@@ -30,8 +29,9 @@ authors_query = '''
 
 
 error_query = '''
-            SELECT total_req.day, (total_err.num/total_req.num)*100 AS ErrorPercentage
-            FROM total_req 
+            SELECT total_req.day,
+                   (total_err.num/total_req.num)*100 AS ErrorPercentage
+            FROM total_req
             JOIN total_err
             ON total_req.day = total_err.day
             WHERE (total_err.num/total_req.num)*100 > 1;
@@ -79,4 +79,4 @@ if __name__ == "__main__":
     top_articles()
     top_authors()
     error_day()
-    print('\n' + "**************************************")
+    print("**************************************")
